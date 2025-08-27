f7814c9c212ffb219d5d02bb6a936ddd47e0ccaa
EduardogDev <eduardosgal14@gmail.com>
2025-08-26 21:17:28 -0300

    Finalizando aula sobre introdução ao JPA

---
+++ src/main/java/dev/jav10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.jav10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+import org.springframework.web.util.pattern.PathPattern;
+
+
+//O ENTITY TRANSFORMA UMA CLASSE EM UMA ENTIDADE PARA BANCO DE DADOS.
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
